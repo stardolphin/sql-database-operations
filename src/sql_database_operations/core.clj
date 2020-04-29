e37121c253510d64cf1cc0511ad5e95cb42a4039
stardolphin <dolphinlink@gmail.com>
2020-04-28 22:45:25 -0600

    Adding DB to .gitignore

--- src/sql_database_operations/core.clj
+++ src/sql_database_operations/core.clj
@@ -1,9 +1,15 @@
 (ns sql-database-operations.core
   (:require
     [sql-database-operations.core :refer :all]
-    [[next.jdbc :as jdbc]]))
+    [next.jdbc :as jdbc]
+    [next.jdbc.result-set :as rs]))
+
+(def db {:dbtype "h2" :dbname "example"})
+(def ds (jdbc/get-datasource db))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+;; https://github.com/seancorfield/next-jdbc/blob/master/doc/getting-started.md
